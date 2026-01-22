896195ce8d42680b5ac87dd5c014856bea9dd4f0
Kant <quent92100@gmail.com>
2026-01-22 01:09:31 +0100

    feat: update readFile and readFileHeader to return Uint8Array; use minicbor-serde dependency

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,11 +1,17 @@
+import { CborDecoder } from "@jsonjoy.com/json-pack/lib/cbor";
 import test from "ava";
 import { readFile, readFileHeader } from "../index";
+
+function parseResult(res: Uint8Array) {
+  const dec = new CborDecoder();
+  return dec.decode(res);
+}
 
 test("readFile from native", (t) => {
   const res = readFile(
     "./__test__/demos/61F74DFC4C42C4FB11F1FABECB0C561B.replay",
   );
-  const json = JSON.parse(res);
+  const json = parseResult(res);
   console.log(json);
   t.truthy(json);
   t.like(json, {
@@ -20,7 +26,7 @@ test("readFileHeader from native", (t) => {
   const res = readFileHeader(
     "./__test__/demos/61F74DFC4C42C4FB11F1FABECB0C561B.replay",
   );
-  const json = JSON.parse(res);
+  const json = parseResult(res);
   console.log(json);
   t.truthy(json);
   t.like(json, {
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![deny(clippy::all)]
 use boxcars::{ParseError, Replay};
-use std::fs;
+use minicbor_serde;
+use napi::bindgen_prelude::Uint8Array;
 use napi_derive::napi;
+use std::fs;
 
 fn parse_rl(data: &[u8]) -> Result<Replay, ParseError> {
   boxcars::ParserBuilder::new(data)
@@ -17,15 +19,17 @@ fn parse_rl_header(data: &[u8]) -> Result<Replay, ParseError> {
 }
 
 #[napi]
-pub fn read_file(filename: String) -> napi::Result<String> {
+pub fn read_file(filename: String) -> napi::Result<Uint8Array> {
   let buffer = fs::read(filename).map_err(|e| napi::Error::from_reason(e.to_string()))?;
   let replay = parse_rl(&buffer).map_err(|e| napi::Error::from_reason(e.to_string()))?;
-  serde_json::to_string(&replay).map_err(|e| napi::Error::from_reason(e.to_string()))
+  let vec = minicbor_serde::to_vec(&replay).map_err(|e| napi::Error::from_reason(e.to_string()))?;
+  Ok(Uint8Array::from(vec))
 }
 
 #[napi]
-pub fn read_file_header(filename: String) -> napi::Result<String> {
+pub fn read_file_header(filename: String) -> napi::Result<Uint8Array> {
   let buffer = fs::read(filename).map_err(|e| napi::Error::from_reason(e.to_string()))?;
   let replay = parse_rl_header(&buffer).map_err(|e| napi::Error::from_reason(e.to_string()))?;
-  serde_json::to_string(&replay).map_err(|e| napi::Error::from_reason(e.to_string()))
+  let vec = minicbor_serde::to_vec(&replay).map_err(|e| napi::Error::from_reason(e.to_string()))?;
+  Ok(Uint8Array::from(vec))
 }
